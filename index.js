8d93af4b74ae74261aa84b1dc611f901b957658b
jinqiming <jinqiming@didichuxing.com>
2019-02-02 19:50:57 +0800

    add open/close

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ logsystem.set_logging({})
 
 var core = new Core()
 logsystem.set_logging('debug')
+
+isEnabled = true
+
 core.registerMessageHandler(1,async function(msg){
 	fromUser = msg.From
 	toUser = msg.To
@@ -16,13 +19,25 @@ core.registerMessageHandler(1,async function(msg){
 		return true
 	}
 
+
 	if('filehelper'==toUser.UserName){
-		replyContent = `[托管中]${this.get_showname(toUser)}您好,${this.get_myNickName()}已经收到您的消息，请您稍等，不要拉黑，不要焦虑，马上回复您，建议您急事直接打电话15858178942.`
+		let replyContent = "[DONE]"
+		if(content == '关闭'){
+			isEnabled = false
+		} else if (content == '打开') {
+			isEnabled = true
+		} else {
+			replyContent = `[托管中]${this.get_showname(toUser)}您好,${this.get_myNickName()}已经收到您的消息，请您稍等，不要拉黑，不要焦虑，马上回复您，建议您急事直接打电话15858178942.`
+		}
+
 		let issucc = await this.reply_to('filehelper',replyContent)
 		console.log('Reply ' + (issucc?'OK':'FAIL'))
 		return issucc
 	}
 
+	if(!isEnabled){
+		return true
+	}
 
 	replyContent = `[托管中]${this.get_showname(fromUser)}您好,${this.get_myNickName()}已经收到您的消息，请您稍等，不要拉黑，不要焦虑，马上回复您，建议您急事直接打电话15858178942.`
 	let issucc = await this.reply(msg,replyContent)
